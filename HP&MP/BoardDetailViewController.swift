62bfe2ae98dcd0df03ddfd3cf9ed33e47c7c54ce
guru2_5 <beans_bin@naver.com>
2020-08-18 21:00:35 +0900

    게시판 탭 : 디자인에 맞게 레이아웃 변경, 모든 action 및 outlet 연결, segue 연결, 테이블 뷰 구현

---
+++ HP&MP/BoardDetailViewController.swift
@@ -0,0 +1,46 @@
+//
+//  BoardDetailViewController.swift
+//  HP&MP
+//
+//  Created by 박예빈 on 2020/08/18.
+//  Copyright © 2020 GURU2. All rights reserved.
+//
+
+import UIKit
+
+class BoardDetailViewController: UIViewController{
+    
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var nicknameLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var recommendLabel: UILabel!
+    @IBOutlet weak var textLabel: UITextView!
+    
+    @IBOutlet weak var idField: UITextField!
+    @IBOutlet weak var pwField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+    // 추천 버튼
+    @IBAction func recommend(_ sender: Any) {
+              
+          }
+    // 수정 버튼
+       @IBAction func modify(_ sender: Any) {
+                 
+             }
+    // 삭제 버튼
+       @IBAction func del(_ sender: Any) {
+                 
+             }
+    
+    
+}
--- HP&MP/BoardViewController.swift
+++ HP&MP/BoardViewController.swift
@@ -9,28 +9,73 @@
 import UIKit
 import Firebase
 
-class BoardViewController: UIViewController {
+class BoardViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     let db = Database.database().reference()
     
+    let titleList = ["근력 운동 식단", "다이어트 식단", "단백질 위주 식단", "삼시세끼 식단", "낮은 열량 식단"]
+    let recommendList = ["10", "15", "34", "1", "24"]
+    let nicknameList = ["몽글이", "자몽이", "별이", "콩이", "정크랫"]
+    let timeList = ["10:48", "3:48", "2:22", "8:00", "7:54"]
+    
+    
+    // 화면이 메모리에 로드되기 전에 수행되어야 할 작업
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
         // ovserceSingleEvnet : 서버로부터 데이터를 한 번 읽을 때 사용
         db.child("First").observeSingleEvent(of: .value) { snapshot in
             print("--> \(snapshot)")
         }
     
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+    // UITableViewDataSource : 몇 개 보여줄지?
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titleList.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else {
+            return UITableViewCell()
+        }
+
+        cell.titleLabel.text = titleList[indexPath.row]
+        cell.recommendLabel.text = recommendList[indexPath.row]
+        cell.nicknameLabel.text = nicknameList[indexPath.row]
+        cell.timeLabel.text = timeList[indexPath.row]
+        
+        print("\(String(describing: cell.nicknameLabel.text))")
+        
+        return cell
+    }
+    
+    // UITableViewDelegate : 셀 클릭 시 동작 설정
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("--> \(indexPath.row)")
+        performSegue(withIdentifier: "showDetail", sender: indexPath.row)
+    }
+    
+    
+    
+    // 글쓰기 버튼
+    @IBAction func write(_ sender: Any) {
+        performSegue(withIdentifier: "showWrite", sender: (Any).self)
+    }
+    
+    // Best, New Control
+    @IBAction func segment(_ sender: Any) {
+        
+    }
 
 }
+
+class ListCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var recommentImgView: UIImageView!
+    @IBOutlet weak var recommendLabel: UILabel!
+    @IBOutlet weak var nicknameLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+}
+
+
