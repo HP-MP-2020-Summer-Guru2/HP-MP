5dec00f8216c2fb155ce073e53b93a547b36c84b
jane1choi <eunjuzzoo@naver.com>
2020-08-18 18:13:47 +0900

    Revert "Merge branch 'master' of https://github.com/beansbin/HP-MP"
    This reverts commit 5ace8a618daba3c5d53c1fbf81c2dac986570ad4, reversing
    changes made to e3675c3dec196e900d57f4ce244ac449ca187c95.

--- HP&MP/FirstViewController.swift
+++ HP&MP/FirstViewController.swift
@@ -13,7 +13,6 @@ class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-
     }
     
     func abc () {
