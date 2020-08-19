45902cd168ae45f5b8a8f7592bf8bf0d2925a5bf
yys <yys88699@naver.com>
2020-08-20 02:07:05 +0900

    firebase compile 오류 확인

--- HP&MP/FirstViewController.swift
+++ HP&MP/FirstViewController.swift
@@ -7,19 +7,37 @@
 //
 
 import UIKit
-
+//500 = 0.5L
+//2L
 class FirstViewController: UIViewController {
-    @IBOutlet var waterFlow: UIImageView!
+    var cupWater: Double = 0.5 //한 컵 기준
+    var todayWater: Double = 0.0 //시작
+    var myWater: Double? // button 누를때 마다 변함
     @IBOutlet var numImageView1: UIImageView!
     @IBOutlet var SleepButton: UIButton!
+    @IBOutlet var waterButton: UIButton!
+    @IBOutlet var totalAmountOfWater: UILabel!
+    @IBAction func minusClick(_ sender: Any) {
+        totalAmountOfWater.text = "바뀌었는지 확인"
+    }
+    
+    @IBAction func waterButtonClicked(_ sender: UIButton) {
+        //if문 검사하는 넣기 , todaywater == 2L면 안차오르게 or 다 마셨어요
 
+        
+//         else           이 위치에 차오르게 하기
+        // todayWater += 0.5
+        
+            self.waterButton.transform = CGAffineTransform(translationX: 0, y: -40)
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        SleepButton.layer.cornerRadius = SleepButton.frame.size.width
-        
-        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapAnimations)))
+//        SleepButton.layer.cornerRadius = SleepButton.frame.size.width
+//
+//        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapAnimations)))
 
     }
     
@@ -28,26 +46,26 @@ class FirstViewController: UIViewController {
 //    }
     
     
-    @objc fileprivate func handleTapAnimations() {
-        
-    
-        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
-            
-            self.waterFlow.transform = CGAffineTransform(translationX: 0, y: -40)
-            
-        }) { (_) in
-            }
-            
-            
-            
-//            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+//    @objc fileprivate func handleTapAnimations() {
 //
-//                self.waterFlow.transform = self.waterFlow.transform.translatedBy(x: 0, y: -150)
 //
-//            })
-
-            
-        }
+//        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+//
+//            self.waterFlow.transform = CGAffineTransform(translationX: 0, y: -40)
+//
+//        }) { (_) in
+//            }
+//
+//
+//
+////            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+////
+////                self.waterFlow.transform = self.waterFlow.transform.translatedBy(x: 0, y: -150)
+////
+////            })
+//
+//
+//        }
     }
     
     
