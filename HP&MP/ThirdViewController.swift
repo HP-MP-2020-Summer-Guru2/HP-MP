d43f776526d8e47d3e078c50bfe21f3e7b664b49
jane1choi <eunjuzzoo@naver.com>
2020-08-20 14:44:26 +0900

    화면전환구현
    1

--- HP&MP/ThirdViewController.swift
+++ HP&MP/ThirdViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ThirdViewController: UIViewController {
 
+    @IBOutlet var mainView: UIView!
+    @IBOutlet var sleepView: UIView!
+    @IBOutlet var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background.png")!)
+        sleepView.isHidden = true
 
         // Do any additional setup after loading the view.
     }
@@ -21,7 +26,33 @@ class ThirdViewController: UIViewController {
         guard let url = URL(string: "https://youtu.be/sXLCkQnn1vc"), UIApplication.shared.canOpenURL(url) else { return }
          UIApplication.shared.open(url, options: [:], completionHandler: nil)
         }
+    
+    @IBAction func restButton(_ sender: UIButton) {
+        guard let url = URL(string: "https://youtu.be/dZewQEbQQM0"), UIApplication.shared.canOpenURL(url) else { return }
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
     }
+    
+    @IBAction func songButton(_ sender: UIButton) {
+        guard let url = URL(string: "https://youtu.be/gbW1QFScQmE"), UIApplication.shared.canOpenURL(url) else { return }
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func asmrButton(_ sender: UIButton) {
+        guard let url = URL(string: "https://youtu.be/qg9Ek28fwHk"), UIApplication.shared.canOpenURL(url) else { return }
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func yogaButton(_ sender: UIButton) {
+        guard let url = URL(string: "https://youtu.be/Yi_toDN2cSM"), UIApplication.shared.canOpenURL(url) else { return }
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    }
+    @IBAction func chooseSleep(_ sender: UIButton) {
+        sleepView.isHidden = false
+        mainView.isHidden = true
+        scrollView.isHidden = true
+    }
+}
+
     
     /*
     // MARK: - Navigation
