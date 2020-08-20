7526c1ddea3872d1b99aa470a84cff25c0d715d5
jane1choi <eunjuzzoo@naver.com>
2020-08-20 13:20:48 +0900

    스크롤뷰구현
    commit

--- HP&MP/ThirdViewController.swift
+++ HP&MP/ThirdViewController.swift
@@ -12,6 +12,7 @@ class ThirdViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background.png")!)
 
         // Do any additional setup after loading the view.
     }
