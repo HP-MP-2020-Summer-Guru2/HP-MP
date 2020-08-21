0242d43dbefd22d8ae548d2e7ad85a3aea30f27b
YujinMyeong <habibiyujin@gmail.com>
2020-08-21 17:57:38 +0900

    앱로고 수정

--- HP&MP/AppDelegate.swift
+++ HP&MP/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        sleep(2)    // 스플래시화면 2초 지연
+        sleep(5)    // 스플래시화면 2초 지연
         // Override point for customization after application launch.
         FirebaseApp.configure()
         return true
